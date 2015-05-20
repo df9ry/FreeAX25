c92a94584e8bc37f405844f68f470e11fd31e5f2
Tania Knoebl <Tania@DF9RY.de>
2015-05-20 18:07:39 +0200

    Added license header to files

--- FreeAX25.cpp
+++ FreeAX25.cpp
@@ -1,10 +1,20 @@
-//============================================================================
-// Name        : OpenAX25_V2.cpp80
-// Author      : Tania Knoebl (DF9RY)
-// Version     :
-// Copyright   : This project is contributed under the GPL v 3 license
-// Description : Hello World in C, Ansi-style
-//============================================================================
+/*
+	Project FreeAX25
+    Copyright (C) 2015  tania@df9ry.de
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "XMLRuntime.h"
 #include "Utils.h"
